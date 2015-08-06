f7a9c7cba50581428fd5c68dfa695e0bcbf91bb0
Eric M <ericmarchese77@gmail.com>
2015-08-06 18:11:55 -0400

    Added account_list, fixed errors

--- model.rb
+++ model.rb
@@ -1,6 +1,7 @@
 require 'sqlite3'
 
 class Bank
+	attr_accessor :db
 	def initialize
 		initialize_database
 	end
@@ -8,8 +9,8 @@ class Bank
 	# Creates database file if it doesn't exist, sets results to be outputted as hashes, and creates tables if they don't yet exist
 	def initialize_database
 		@db = SQLite3::Database.new("bank.db")
-		@db.results_as_hash = true
-		accounts_table_exists = @db.execute("SELECT 1 FROM sqlite_master WHERE type='table' AND name= ?", "accounts").length > 0
+		db.results_as_hash = true
+		accounts_table_exists = db.execute("SELECT 1 FROM sqlite_master WHERE type='table' AND name= ?", "accounts").length > 0
 		unless accounts_table_exists
 			create_accounts_table
 			create_managers_table
@@ -18,7 +19,7 @@ class Bank
 
 	# Creates accounts table in database file
 	def create_accounts_table
-		@db.execute %q{
+		db.execute %q{
 			CREATE TABLE accounts (
 		  id integer primary key,
 		  name varchar(50),
@@ -29,7 +30,7 @@ class Bank
 
 	# Creates managers table in database file
 	def create_managers_table
-		@db.execute %q{
+		db.execute %q{
 			CREATE TABLE managers (
 		  id integer primary key,
 		  name varchar(50),
@@ -39,32 +40,38 @@ class Bank
 
 	# Adds a new manager
 	def add_manager(name, pin)
-		@db.execute("INSERT INTO managers (name,pin) VALUES (?,?)", name, pin)
+		db.execute("INSERT INTO managers (name,pin) VALUES (?,?)", name, pin)
 	end
 
 	# Adds new account with given name and pin, sets balance to 0
 	def add_account(name, pin)
-		@db.execute("INSERT INTO accounts (name,balance,pin) VALUES (?,?,?)", name, 0, pin)
+		db.execute("INSERT INTO accounts (name,balance,pin) VALUES (?,?,?)", name, 0, pin)
 	end
 
 	# Returns true if name and pin match are found or false if not found; table parameter should be either "accounts" or "managers"
 	def verify_pin(name, pin, table)
-		@db.execute("SELECT 1 FROM ? WHERE name = ? AND pin = ?", table, name, pin).length > 0
+		db.execute("SELECT 1 FROM #{table} WHERE name = ? AND pin = ?", name, pin).length > 0
+	end
+
+	# Returns list of all bank accounts as an array of hashes. Requires manager name and pin match to be true
+	def account_list(name, pin)
+		list = db.execute("SELECT * FROM accounts")
+		return list if verify_pin(name, pin, "managers")
 	end
 
 	# Returns balance for an account
 	def balance(name, pin)
-		customer = @db.execute("SELECT * FROM accounts WHERE name = ? AND pin = ?", name, pin)
+		customer = db.execute("SELECT * FROM accounts WHERE name = ? AND pin = ?", name, pin)
 		return customer[0]["balance"]
 	end
 
 # Subtracts amount from balance of an account
 	def withdraw(name, pin, amount)
-		@db.execute("UPDATE accounts SET balance = balance - ? WHERE name = ? AND pin = ?", amount, name, pin)
+		db.execute("UPDATE accounts SET balance = balance - ? WHERE name = ? AND pin = ?", amount, name, pin)
 	end
 
 # Add amount to balance of an account
 	def deposit(name, pin, amount)
-		@db.execute("UPDATE accounts SET balance = balance + ? WHERE name = ? AND pin = ?", amount, name, pin)
+		db.execute("UPDATE accounts SET balance = balance + ? WHERE name = ? AND pin = ?", amount, name, pin)
 	end
 end
