4d44a7027d5cd528d004989312596f42ab111b04
Eric M <ericmarchese77@gmail.com>
2015-08-09 22:09:53 -0400

    Added more methods, refactored classes

--- model.rb
+++ model.rb
@@ -7,12 +7,14 @@ class Bank
 		initialize_database
 	end
 
-	# Creates database file if it doesn't exist, sets results to be outputted as hashes, turns on foreign key support, and creates tables if they don't yet exist
+	# Creates database file if it doesn't exist, sets results to be outputted as
+	# hashes, turns on foreign key support, and creates tables if they don't exist
 	def initialize_database
 		@db = SQLite3::Database.new("bank.db")
 		db.results_as_hash = true
 		db.execute("PRAGMA foreign_keys = ON")
-		accounts_table_exists = db.execute("SELECT 1 FROM sqlite_master WHERE type='table' AND name= ?", "accounts").length > 0
+		accounts_table_exists = db.execute("SELECT 1 FROM sqlite_master WHERE 
+			type='table' AND name= ?", "accounts").length > 0
 		unless accounts_table_exists
 			create_customers_table
 			create_accounts_table
@@ -25,8 +27,8 @@ class Bank
 		db.execute %q{
 			CREATE TABLE accounts (
 		  account_id integer primary key,
-		  customer_id integer references customers(customer_id) ON UPDATE CASCADE ON DELETE CASCADE,
-		  name varchar(50),
+		  customer_id integer references customers(customer_id) ON UPDATE CASCADE ON
+		   DELETE CASCADE,
 		  balance float)
 		}
 	end
@@ -56,64 +58,127 @@ class Bank
 		db.execute("INSERT INTO managers (name,pin) VALUES (?,?)", name, pin)
 	end
 
-	# Adds a new customer to database
-	def add_customer(name, pin)
-		db.execute("INSERT INTO customers (name,pin) VALUES (?.?)", name, pin)
-	end
-
-	# Adds new account under given customer_id, sets balance to 0
-	def add_account(customer_id)
-		db.execute("INSERT INTO accounts (customer_id,balance) VALUES (?,?)", customer_id, 0)
-	end
-
-	# Returns true if name and pin match are found or false if not found; table parameter should be either "accounts" or "managers"
+	# Returns true if name and pin match are found or false if not found; table 
+	# parameter should be either "customers" or "managers"
 	def verify_pin(name, pin, table)
-		db.execute("SELECT 1 FROM #{table} WHERE name = ? AND pin = ?", name, pin).length > 0
+		db.execute("SELECT 1 FROM #{table} WHERE name = ? AND pin = ?", 
+			name, pin).length > 0
 	end
 
-	# Returns list of all bank accounts as an array of hashes. Requires manager name and pin match to be true
+	# Returns list of all bank accounts as an array of hashes. Requires manager 
+	# name and pin match to be true
 	def account_list(name, pin)
 		list = db.execute("SELECT * FROM accounts")
 		return list if verify_pin(name, pin, "managers")
 	end
+
+	# Returns true if name exists in given table; table parameter should be either
+	# "customers" or "managers"
+	def name_exists?(name, table)
+		db.execute("SELECT 1 FROM #{table} WHERE name = ?", 
+			name).length > 0
+	end
+
+	# Returns customer_id of customer found with name and pin
+	def find_customer_id(name, pin)
+		customer = db.execute("SELECT * FROM customers WHERE name = ? AND pin = ?", 
+			name, pin)
+		return customer[0]["customer_id"]
+	end
+
+	# Returns accounts for customer
+	def customer_accounts(name, pin)
+		db.execute("SELECT * FROM accounts WHERE customer_id = ?", find_customer_id(
+			name, pin))
+	end
+
+	# Returns customer found with given customer_id
+	def find_customer_by_customer_id(customer_id)
+		db.execute("SELECT * FROM customers WHERE customer_id = ?",
+			customer_id)
+	end
+
+	# Returns Customer instance with customer from database
+	def load_customer(customer_id)
+		customer = find_customer_by_customer_id(customer_id)
+		Customer.new(db, customer[0]["name"], customer[0]["pin"])
+	end
+
+	# Returns Account instance with account from database
+	def load_account(account_id)
+		account = db.execute("SELECT * FROM accounts WHERE account_id = ?",
+			account_id)
+		Account.new(db, account[0]["customer_id"], false)
+	end
 end
 
 class Customer
-	attr_accessor :db, :name, :pin, :balance
+	attr_accessor :db, :name, :pin
 
 	def initialize(db, name, pin)
 		@db = db
 		@name = name
 		@pin = pin
-		@balance = 0
 	end
 
-	def save_to_db
-		#
+	def add_to_db
+		db.execute("INSERT INTO customers (name,pin) VALUES (?,?)", name, pin)
 	end
 end
 
 class Account
-	attr_accessor :db, :customer_id
+	attr_accessor :db, :balance, :new_account, :customer_id
 
-	def initialize(customer_id, db)
+	def initialize(db, customer_id, new_account = true)
 		@customer_id = customer_id
+		@balance = 0.0
 		@db = db
+		@new_account = new_account
+	end
+
+	# Subtracts amount from balance of account
+	def withdraw(name, pin, amount)
+		db.execute("UPDATE accounts SET balance = balance - ? WHERE name = ? AND pin
+		 = ?", amount, name, pin)
+	end
+
+	# Add amount to balance of account
+	def deposit(name, pin, amount)
+		db.execute("UPDATE accounts SET balance = balance + ? WHERE name = ? AND pin
+		 = ?", amount, name, pin)
 	end
 
 	# Returns balance for an account
-	def balance(name, pin)
-		customer = db.execute("SELECT * FROM accounts WHERE name = ? AND pin = ?", name, pin)
+	def return_balance(customer_id)
+		customer = db.execute("SELECT * FROM accounts WHERE customer_id = ?", 
+			customer_id)
 		return customer[0]["balance"]
 	end
 
 	# Subtracts amount from balance of an account
-	def withdraw(name, pin, amount)
-		db.execute("UPDATE accounts SET balance = balance - ? WHERE name = ? AND pin = ?", amount, name, pin)
-	end
-
-	# Add amount to balance of an account
-	def deposit(name, pin, amount)
-		db.execute("UPDATE accounts SET balance = balance + ? WHERE name = ? AND pin = ?", amount, name, pin)
+	def save_to_db
+		if new_account
+			db.execute("INSERT INTO accounts (customer_id,balance) VALUES (?,?)", 
+				customer_id, balance)
+		else
+			db.execute("UPDATE accounts SET balance = #{balance} WHERE customer_id = 
+				?", customer_id)
+		end
 	end
 end
+
+
+# Testing stuff.....
+
+# bank = Bank.new
+# bank.add_customer("Bob", 1234)
+# cust_id = bank.find_customer_id("Bob", 1234)
+# 	puts cust_id
+# bank.add_account(cust_id)
+# acc = Account.new(cust_id, bank.db)
+# acc.balance = 500
+# 	puts acc.balance
+# acc.save_to_db
+# 	puts acc.return_balance(cust_id)
+# bank.add_manager("Joe the plumber", 1234)
+# puts bank.account_list("Joe the plumber",1234).count
